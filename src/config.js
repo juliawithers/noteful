6576bae041fe3f9e4031f5f2ecc7e6d1e25fd955
Julia Fehr <09jwithers@gmail.com>
2020-05-25 17:01:44 -0400

    getting ready for deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
 export default {
-    API_FOLDERS_ENDPOINT: `http://localhost:8000/api/noteful/folders`,
-    API_NOTES_ENDPOINT: `http://localhost:8000/api/noteful/notes`,
-    API_NOTEID_ENDPOINT: `http://localhost:8000/api/noteful/notes/:noteId`
+    // API_FOLDERS_ENDPOINT: `http://localhost:8000/api/noteful/folders`,
+    // API_NOTES_ENDPOINT: `http://localhost:8000/api/noteful/notes`,
+    // API_NOTEID_ENDPOINT: `http://localhost:8000/api/noteful/notes/:noteId`
+
+    API_FOLDERS_ENDPOINT: `https://fierce-dawn-33726.herokuapp.com/folders`,
+    API_NOTES_ENDPOINT: `https://fierce-dawn-33726.herokuapp.com/notes`,
+    API_NOTEID_ENDPOINT: `https://fierce-dawn-33726.herokuapp.com/notes/:noteId`
+    // `https://fierce-dawn-33726.herokuapp.com/`
   }
